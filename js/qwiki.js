9b5d8bb283d7334c49646047cb6a61f5e236dcb3
Simon L. Vreeken <svreeken@hellodoc.tk>
2020-03-10 10:35:42 +0100

    removed lookbehind regexp when not on chromium

--- js/qwiki.js
+++ js/qwiki.js
@@ -1,21 +1,26 @@
-var resolver = 1; // improve later with a query parameter
+<!--
 
-;// this script loog for a div of class md
-;// and replace its content with the render markdown
-;// obtained from a gradualpad.
-;//
-;// The pad name is derived from data-gradualid
-;//
-;// and then replace all the [[wikilinks]]
-;// with a "onclick" resolver to access the url from
-;// the [[blockRing]] index.
-;//
-;//
-;// when page is loaded it post itself under its own pagename
-;// indexes are merged w/i the neigborhood (i.e. trusted friend level 1)
-;// 
-;// indexes are sharded according to a truncate hash,
-;// to optimize access speed and size
+var resolver = 1; /* improve later with a query parameter */
+// Chrome 1 - 79
+var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+var lookbehind = (isChrome) ? 1 : 0;
+
+// this script loog for a div of class md
+// and replace its content with the render markdown
+// obtained from a gradualpad.
+//
+// The pad name is derived from data-gradualid
+//
+// and then replace all the [[wikilinks]]
+// with a "onclick" resolver to access the url from
+// the [[blockRing]] index.
+//
+//
+// when page is loaded it post itself under its own pagename
+// indexes are merged w/i the neigborhood (i.e. trusted friend level 1)
+// 
+// indexes are sharded according to a truncate hash,
+// to optimize access speed and size
 
 var branding = 'frama' 
 const url = window.location.href
@@ -24,12 +29,12 @@ if (url.match(/holo/i)) { branding = 'holo' }
 else if (url.match(/\.gq/)) { branding = 'gradual' } 
 else if (url.match(/oogle/)) { branding = 'yoogle' } 
 else if (url.match(/blockring/)) { branding = 'brings' } 
-else if (url.match(/qwiki/i)) { branding = 'q' }
 else if (url.match(/\.ml/)) { branding = 'myc' } 
 else if (url.match(/cloud/)) { branding = 'cloud' } 
 else if (url.match(/ipfs/)) { branding = 'ipfs' } 
 else if (url.match(/ipms/)) { branding = 'ipms' } 
 else if (url.match(/127/)) { branding = 'local' } 
+else if (url.match(/qwiki/i)) { branding = 'q' }
 
 var brand;
 var gitrepo;
@@ -130,12 +135,23 @@ function render(e) {
    md = md.replace(/%loc%/g,document.location);
    md = md.replace(/%hostname%/g,document.location.hostname);
    md = md.replace(/%origin%/g,document.location.origin);
-   md = md.replace(/(?<!'){{DUCK}}/g,'https://duckduckgo.com/?q');
-   md = md.replace(/(?<!'){{QWANT}}/g,'https://qwant.com/?q');
-   md = md.replace(/(?<!'){{LILO}}/g,'https://search.lilo.org/results.php?openvialilo=true&q');
-   md = md.replace(/(?<!'){{START}}/g,'https://www.startpage.com/do/search?query');
-   md = md.replace(/(?<!'){{chart}}/g,'https://chart.apis.google.com/chart?cht=qr&chs=240x240&chl');
-   md = md.replace(/(?<!'){{citeas}}/g,'"[['+pageName+'](https://gradual-quanta.github.io/qwiki/#'+pageName+')]: <https://gradual-quanta.github.io/qwiki/#'+pageName+'>"');
+   if (lookbehind) { /* Lookbehinds are not supported by Firefox and other browsers like IE.
+   see [*](https://caniuse.com/#feat=js-regexp-lookbehind) */
+   md = md.replace(RegExp("(?<!'){{DUCK}}",'g'),'https://duckduckgo.com/?q');
+   md = md.replace(RegExp("(?<!'){{QWANT}}",'g'),'https://qwant.com/?q');
+   md = md.replace(RegExp("(?<!'){{LILO}}",'g'),'https://search.lilo.org/results.php?openvialilo=true&q');
+   md = md.replace(RegExp("(?<!'){{START}}",'g'),'https://www.startpage.com/do/search?query');
+   md = md.replace(RegExp("(?<!'){{chart}}",'g'),'https://chart.apis.google.com/chart?cht=qr&chs=240x240&chl');
+   md = md.replace(RegExp("(?<!'){{citeas}}",'g'),'"[['+pageName+'](https://gradual-quanta.github.io/qwiki/#'+pageName+')]: <https://gradual-quanta.github.io/qwiki/#'+pageName+'>"');
+   } else {
+   console.log('WARNING: lookbehind: (?<!\') not supported, please use brave !')
+   md = md.replace(/{{DUCK}}(?!')/g,'https://duckduckgo.com/?q');
+   md = md.replace(/{{QWANT}}(?!')/g,'https://qwant.com/?q');
+   md = md.replace(/{{LILO}}(?!')/g,'https://search.lilo.org/results.php?openvialilo=true&q');
+   md = md.replace(/{{START}}(?!')/g,'https://www.startpage.com/do/search?query');
+   md = md.replace(/{{chart}}(?!')/g,'https://chart.apis.google.com/chart?cht=qr&chs=240x240&chl');
+   md = md.replace(/{{citeas}}(?!')/g,'"[['+pageName+'](https://gradual-quanta.github.io/qwiki/#'+pageName+')]: <https://gradual-quanta.github.io/qwiki/#'+pageName+'>"');
+   }
    var htm = markdownify(e,md);
    
  }
@@ -187,10 +203,18 @@ function wikilinks(e,buf) {
   rex = RegExp(/\[pageName\](?![('\[\]])/,'g'); // [{{pageName}}]
   buf = buf.replace(rex,"<a href=\"https://qwant.com/?q=%26g++%22pageName%22\">pageName</a>");
 
-  rex = RegExp(/(?<!['"\/])#(\w+)(?!['">])/,'g'); // #hashtag
+  if (lookbehind) {
+  rex = RegExp('(?<![\'"\/])#(\w+)(?![\'">])','g'); // #hashtag
   buf = buf.replace(rex,"<a target=\"$1\" href=\"https://qwant.com/?q=%26g+%23$1\">#$1</a>");
-  rex = RegExp(/(?<!['\[\]])\[([^\]=]*?)\](?![('\[\]])/,'g'); // [localpage]
+  rex = RegExp('(?<![\'\[\]])\[([^\]=]*?)\](?![(\'\[\]])','g'); // [localpage]
   buf = buf.replace(rex,"<a href=\"#$1\">$1</a>");
+  } else {
+  console.log('WARNING: lookbehind: (?<!\') not supported, please use a chromium browser !')
+  rex = RegExp(/#(\w+)(?!['">])/,'g'); // #hashtag
+  buf = buf.replace(rex,"<a target=\"$1\" href=\"https://qwant.com/?q=%26g+%23$1\">#$1</a>");
+  rex = RegExp(/\[([^\]=]*?)\](?![('\[\]])/,'g'); // [localpage]
+  buf = buf.replace(rex,"<a href=\"#$1\">$1</a>");
+  }
   //buf = buf.replace(rex,"<a href=\"https://mensuel.framapad.org/p/$1\">$1</a>");
   }
   e.innerHTML = buf;
@@ -233,3 +257,4 @@ function logError(e) {
   console.log(e)
 }
 
+-->
